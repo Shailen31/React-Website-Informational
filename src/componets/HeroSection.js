5b4cb6a44801856d10d1f77ac3de2f14953fc3c0
Shailen Narainsamy <shailen.narainsamy@derivco.co.za>
2022-09-28 19:34:19 +0200

    Changed Background

--- src/componets/HeroSection.js
+++ src/componets/HeroSection.js
@@ -6,7 +6,8 @@ import '../App.css'
 function HeroSection() {
     return (
         <div className = 'hero-container'>
-            <video src = "/videos/video-1.mp4" autoPlay loop muted />
+            {/* <video src = "/videos/video-2.mp4" autoPlay loop muted /> */}
+            {/* <img src="/images/wallpaper1.webp" alt="Wallpaper" />; */}
             <h1>ADVENTURE AWAITS</h1>
             <p>What are you waiting for??</p>
 

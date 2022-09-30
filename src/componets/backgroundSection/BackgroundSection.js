fc30b89386d15eefda2c19bae7587e2ec5080969
Micara Ramnanan <Micara.Ramnanan@Altron.com>
2022-10-01 00:09:17 +0200

    services

--- src/componets/backgroundSection/BackgroundSection.js
+++ src/componets/backgroundSection/BackgroundSection.js
@@ -4,7 +4,27 @@ import "./BackgroundSection.css";
 function BackgroundSection() {
   return (
     <>
-      <div>Background</div>
+      <div class="background-container">
+        <div class="div1"></div>
+        <div class="div2">
+          <h1>Greetings & Welcome To My Practice</h1>
+          <h2>
+            I am Varsha Dookie, a social worker in private practice with more
+            than 20 years of experience in individual and group counselling. My
+            passion lies in empowering my clients with skills to develop
+            resilience, successfully manage all their relationships, improve
+            their quality of life, achieve goals and realise their full
+            potential. I looking forward to adding value to your life.
+          </h2>
+          <h2>
+            My experience has been gained in both the NGO as well as the
+            manufacturing sector where my focus has been on employee wellness. I
+            have a master’s degree in play therapy (University of Pretoria) and
+            recently obtained a post graduate qualification in student support
+            from the North West University (Psychology Department).
+          </h2>
+        </div>
+      </div>
     </>
   );
 }
--- src/componets/servicesSection/ServicesSection.js
+++ src/componets/servicesSection/ServicesSection.js
@@ -4,7 +4,48 @@ import "./ServicesSection.css";
 function ServicesSection() {
   return (
     <>
-      <div>Services</div>
+      <div class="flexbox-container">
+        <div class="div">
+          <h1>Psychosocial Counselling & Employee Wellness</h1>
+          <h2>
+            (Face - Face; 6-8 sessions based on the client's individual needs)
+          </h2>
+          <hr class="contact-linebreak"></hr>
+          <h3>Couple Counselling</h3>
+          <li class="list">
+            Relationship enrichment, improving communication, anger management,
+            developing respect, developing common goals, parenting and
+            discipline, separation and reconciliation, pre-marital counselling.
+            The couple are initially consulted with individually and thereafter
+            as a couple.
+          </li>
+          <hr class="contact-linebreak"></hr>
+          <h3>Individual Counselling</h3>
+          <li class="list">
+            Trauma debriefing, depression, anxiety, bereavement and loss,
+            parental guidance, divorce counselling, self esteem, work/life
+            balance, substance addition, stress management, life change
+            adjustment.
+          </li>
+        </div>
+        <div class="div11"></div>
+        <div class="div">
+          <h1>Life coaching</h1>
+          <h2>(Wanting self growth and to transform your)</h2>
+          <li class="list">
+            Able to set goals, however not being able to follow through with
+            these goals?
+          </li>
+          <li class="list">
+            Having big dreams, however being unable to charter the course to get
+            there?
+          </li>
+          <li class="list">
+            Need support as no one believes in you so that you can achieve
+            greatness?
+          </li>
+        </div>
+      </div>
     </>
   );
 }
